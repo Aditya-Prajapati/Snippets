0a63d41f72dfaae7f25db7a26c1dc00b6018008d
Aditya Prajapati <adityaprajapati28@gmail.com>
2024-02-25 12:48:11 +0530

    Create  Sieve of Eratosthenes.cpp

---
+++ Sieve of Eratosthenes.cpp
@@ -0,0 +1,32 @@
+ #include <bits/stdc++.h>
+using namespace std;
+
+class Sieve{
+    public:
+        vector<bool> isPrime;
+        int n;
+    
+        Sieve(int _n){
+            n = _n;
+            isPrime.resize(n+1, true);
+        }
+    
+        void markPrimes(){
+            for (int num = 2; num*num <= n; num++) {
+                if (isPrime[num] == true) {
+                    for (int i = num*num; i <= n; i += num)
+                        isPrime[i] = false;
+                }
+            }
+        }
+};
+
+int main(){
+    // TC - O(n*log(log(n)))
+    // SC - O(n)
+    Sieve s(45);
+    s.markPrimes();
+    cout << s.isPrime[29] << endl;
+    
+    return 0;
+}
